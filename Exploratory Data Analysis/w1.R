dd4ea38312e56b0b44ff91ed189429bf19886e7f
wjjreal <wjjreal@gmail.com>
2020-03-09 20:07:42 -0500

    home w1

--- Exploratory Data Analysis/w1.R
+++ Exploratory Data Analysis/w1.R
@@ -57,3 +57,58 @@ boxplot(subset(state, region=="North Central")$Life.Exp, col="green", main = "No
 library(ggplot2)
 data(mpg)
 qplot(displ, hwy, data=mpg)
+
+library(datasets)
+hist(airquality$Ozone)
+
+with(airquality,plot(Wind, Ozone))
+title(main="Ozone and Wind in New York City")
+
+with(airquality,plot(Wind, Ozone, main="Ozone and Wind in New York City"))
+with(subset(airquality, Month==5), points(Wind, Ozone, col="blue"))
+
+with(airquality,plot(Wind, Ozone, main="Ozone and Wind in New York City"), type="n")
+with(subset(airquality, Month==5), points(Wind, Ozone, col="blue"))
+with(subset(airquality, Month!=5), points(Wind, Ozone, col="red"))
+legend("topright", pch=1, col=c("blue", "red"), legend=c("May", "Others"))
+
+airquality <- transform(airquality, Month=factor(Month))
+boxplot(Ozone ~ Month, airquality, xlab="Month", ylab="Ozone(ppb)")
+
+with(airquality,plot(Wind, Ozone, main="Ozone and Wind in New York City", pch=20))
+model <- lm(Ozone ~ Wind, airquality)
+abline(model, lwd=2)
+
+par(mfrow=c(1,2))
+with(airquality,{
+        plot(Wind, Ozone, main="Ozone and Wind")
+        plot(Solar.R, Ozone, main="Ozone and Solar Radiation")
+})
+
+
+par(mfrow=c(1,3), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(airquality,{
+        plot(Wind, Ozone, main="Ozone and Wind")
+        plot(Solar.R, Ozone, main="Ozone and Solar Radiation")
+        plot(Temp, Ozone, main="Ozone and Temperature")
+        mtext("Ozone and Weather in New York City", outer=TRUE)
+})
+
+
+x <- rnorm(100)
+hist(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
