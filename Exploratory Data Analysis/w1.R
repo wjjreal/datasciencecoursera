b11c9370b85d88b03a7a3ecf12f437836c740dd4
U-ERICSSON\ejigwag <ejigwag@US-00002383.ericsson.se>
2020-03-11 16:38:20 -0500

    project

--- Exploratory Data Analysis/w1.R
+++ Exploratory Data Analysis/w1.R
@@ -58,7 +58,7 @@ library(ggplot2)
 data(mpg)
 qplot(displ, hwy, data=mpg)
 
-<<<<<<< HEAD
+
 library(datasets)
 hist(airquality$Ozone)
 
@@ -162,8 +162,42 @@ dev.copy(png,file="myplot.png")
 dev.off()
 
 
-## swirl
+## project
+temp <- tempfile()
+options(HTTPUserAgent='R http client')
+download.file("https://d396qusza40orc.cloudfront.net/exdata_data_household_power_consumption.zip",temp)
+df <- read.table(unz(temp,"household_power_consumption.txt"),header=TRUE,sep = ";",stringsAsFactors = FALSE)
 
+my_df <- df[as.Date(df$Date,format="%d/%m/%Y")>=as.Date("01/02/2007",format="%d/%m/%Y") & as.Date(df$Date,format="%d/%m/%Y")<=as.Date("02/02/2007",format="%d/%m/%Y"),]
+my_df$datetime <- strptime(paste(my_df$Date,my_df$Time), "%d/%m/%Y %H:%M:%S")
+my_df$Global_active_power <- as.numeric(my_df$Global_active_power)
+my_df$Global_reactive_power <- as.numeric(my_df$Global_reactive_power)
+my_df$Voltage <- as.numeric(my_df$Voltage)
+my_df$Sub_metering_1 <- as.numeric(my_df$Sub_metering_1)
+my_df$Sub_metering_2 <- as.numeric(my_df$Sub_metering_2)
+my_df$Sub_metering_3 <- as.numeric(my_df$Sub_metering_3)
+## plot1
+hist(my_df$Global_active_power,col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)")
 
+## plot2
+with(my_df,plot(datetime,Global_active_power,ylab="Global Active Power (kilowatts)", type="l"))
 
+## plot3
+with(my_df,plot(datetime,Sub_metering_1,ylab="Energy sub metering", type="l"))
+with(my_df,lines(datetime,Sub_metering_2, col="red"))
+with(my_df,lines(datetime,Sub_metering_3, col="blue"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+## plot4
+par(mfrow=c(2,2))
+
+with(my_df,plot(datetime,Global_active_power,ylab="Global Active Power", type="l"))
+
+with(my_df,plot(datetime,Voltage,ylab="Voltage", type="l",xlab="datetime"))
+
+with(my_df,plot(datetime,Sub_metering_1,ylab="Energy sub metering", type="l"))
+with(my_df,lines(datetime,Sub_metering_2, col="red"))
+with(my_df,lines(datetime,Sub_metering_3, col="blue"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+with(my_df,plot(datetime,Global_reactive_power,ylab="Global_reactive_power", type="l",xlab="datetime"))
